f47983ecafe86902513d4e663ad15a0e08458a44
atharva <atharvashailendramehta@gmail.com>
2025-01-02 14:50:13 +0530

    logo and title change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Logo from "../src/Images/345.png";
 import Home from "./Pages/home";
 
 function App() {
   return (
     <>
+      <head>
+        <link rel="icon" type="image/png" href={Logo} />
+        <title>Weater App</title>
+      </head>
       <Home />
     </>
   );
