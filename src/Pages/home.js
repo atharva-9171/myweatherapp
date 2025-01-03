7b2310f70d04772601551efaa518bc19196bd7ea
atharva <atharvashailendramehta@gmail.com>
2025-01-03 11:57:29 +0530

    temrature change commit

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -19,7 +19,7 @@ function Home() {
       console.log(data);
       SetWeatherData({
         humidity: data.main.humidity,
-        temperature: data.main.temp,
+        temperature: Math.floor(data.main.temp),
         location: data.name,
         wind: data.wind.speed,
         icon1: data.weather[0].icon,
